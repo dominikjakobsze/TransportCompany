b9bf7bebc262e65454a1e4857866bc83026af6c0
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-02-12 01:30:45 +0100

    custom login authenticator

---
+++ src/Security/LoginAuthenticator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+
+class LoginAuthenticator extends AbstractAuthenticator
+{
+
+    public function supports(Request $request): ?bool
+    {
+        dd('start');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        // TODO: Implement authenticate() method.
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // TODO: Implement onAuthenticationSuccess() method.
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        // TODO: Implement onAuthenticationFailure() method.
+    }
+}
